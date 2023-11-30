5a1ad799b15e747e364c21090bfc0c6303b88e6b
Ilya Kunitsky <Ilyhhaa@gmail.com>
2023-11-30 15:31:27 +0300

    "All Models and Migration"

---
+++ BookShoppingCartMvcUI/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShoppingCartMvcUI.Models
+{
+    [Table("Book")]
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required] //свойство должно быть обязательно установлено
+        [MaxLength(40)]
+        public string? BookName { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+        
+        public string? Image { get; set; }
+       
+        [Required]
+        public int GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public List<OrderDetail> OrderDetail { get; set; }
+
+        public List<CartDetail> CartDetail { get; set; }
+
+    }
+}
---
+++ BookShoppingCartMvcUI/Models/Genre.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShoppingCartMvcUI.Models
+{
+    [Table("Genre")]
+    public class Genre
+    {
+        
+        
+        
+            public int Id { get; set; }
+
+            [Required] //свойство должно быть обязательно установлено
+            [MaxLength(40)]
+            public string GenreName { get; set; }
+
+            public List<Book> Books { get; set;}
+          
+
+
+        
+    }
+}
