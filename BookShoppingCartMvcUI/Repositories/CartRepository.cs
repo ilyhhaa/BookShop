dcc1e28fe8c33bf9346d035dcefa204279f366ef
Ilya Kunitsky <Ilyhhaa@gmail.com>
2023-12-24 16:23:14 +0300

    "Cart developing and onemore repository"

---
+++ BookShoppingCartMvcUI/Repositories/CartRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace BookShoppingCartMvcUI.Repositories
+{
+    public class CartRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IHttpContextAccessor _httpcontextAccessor;
+        public CartRepository(ApplicationDbContext db, UserManager<IdentityUser> userManager, IHttpContextAccessor HttpcontextAccessor)
+        {
+            _db = db;
+            _userManager = userManager;
+            _httpcontextAccessor = HttpcontextAccessor;
+        }
+        public  async Task<bool> AddItem(int bookId,int qty)
+        {
+            using var transaction = _db.Database.BeginTransaction();
+            try {
+                string userId = GetUserId();
+
+                if (string.IsNullOrEmpty(userId))
+                    return false;
+
+
+
+                var cart = await GetCart(userId);
+                if (cart is null)
+                {
+                    cart = new ShoppingCart
+                    {
+                        UserId = userId,
+                    };
+                    _db.ShoppingCarts.Add(cart);
+                }
+                _db.SaveChanges();
+                var cartItem = _db.CartDetails.FirstOrDefault(x => x.ShoppingCartId ==cart.Id && x.BookId==bookId);
+                if (cartItem is not null)
+                {
+                    cartItem.Quantity += qty;
+                }
+                else
+                {
+                    cartItem = new CartDetail 
+                    {
+                        BookId = bookId,
+                        ShoppingCartId=cart.Id,
+                        Quantity=qty
+                    };
+
+                    _db.CartDetails.Add(cartItem);
+                }
+                _db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private async Task<ShoppingCart> GetCart(string userId) 
+        {
+            var cart = await _db.ShoppingCarts.FirstOrDefaultAsync(x => x.UserId == userId);
+            return cart;
+        }
+        private string GetUserId()
+        {
+            ClaimsPrincipal principal = _httpcontextAccessor.HttpContext.User;
+            string userId = _userManager.GetUserId(principal);
+
+            return userId;
+        }
+    }
+}
