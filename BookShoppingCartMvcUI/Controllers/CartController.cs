5e5035885c864b9bfd0ff06be08b2fb2984a4762
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-03-03 12:18:43 +0300

    Cart Controller

---
+++ BookShoppingCartMvcUI/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShoppingCartMvcUI.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
