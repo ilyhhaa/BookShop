fd6d164915e3c9aeddf86cbf91b56973d594d0e1
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-03-04 09:34:50 +0300

    startCartController

--- BookShoppingCartMvcUI/Controllers/CartController.cs
+++ BookShoppingCartMvcUI/Controllers/CartController.cs
@@ -6,6 +6,13 @@ namespace BookShoppingCartMvcUI.Controllers
     [Authorize]
     public class CartController : Controller
     {
+        private readonly ICartRepository _cartRepo;
+
+        public CartController(ICartRepository cartRepo)
+        {
+            _cartRepo = cartRepo;
+        }
+
         public IActionResult AddItem(int bookId,int qty=1 )
         {
             return View();
