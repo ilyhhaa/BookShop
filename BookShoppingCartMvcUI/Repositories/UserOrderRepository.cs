6b1ffe2c7de379b5b6097109e3f8f003c283a06a
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-04-24 09:52:51 +0300

    Start UserOrderRep IUserRep and addTransient

--- BookShoppingCartMvcUI/Repositories/UserOrderRepository.cs
+++ BookShoppingCartMvcUI/Repositories/UserOrderRepository.cs
@@ -1,6 +1,36 @@
-﻿namespace BookShoppingCartMvcUI.Repositories
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace BookShoppingCartMvcUI.Repositories
 {
     public class UserOrderRepository: IUserOrderRepository
     {
+        private readonly ApplicationDbContext _db;
+        private readonly IHttpContextAccessor _httpcontextAccessor;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserOrderRepository(ApplicationDbContext db, IHttpContextAccessor httpcontextAccessor, UserManager<IdentityUser> userManager)
+        {
+            _db = db;
+            _httpcontextAccessor = httpcontextAccessor;
+            _userManager = userManager;
+        }
+        public async Task<IEnumerable<Order>> UserOrders()
+        {
+            var userId = GetUserId();
+            if (string.IsNullOrEmpty(userId))
+                throw new Exception("User is not logged in");
+            var orders = await _db.Orders.Include("OrderDetail").Where(c=>c.UserId==userId).ToListAsync();
+            return orders;
+        }
+
+        private string GetUserId()
+        {
+            ClaimsPrincipal principal = _httpcontextAccessor.HttpContext.User;
+            var userId = _userManager.GetUserId(principal);
+
+            return userId;
+        }
     }
 }
